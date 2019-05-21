0f9a359125d14fc6af97c523b9d1a850a4d46a8b
dyuzz16 <ciclopej@gmail.com>
2019-05-21 18:38:37 +0300

    Thread with fixed rate

---
+++ src/nightingale/core/nThread.java
@@ -0,0 +1,54 @@
+package nightingale.core;
+
+public class nThread extends java.lang.Thread {
+
+	private Runnable fixRunnable;
+
+	public int maxRate = 60;
+	private int TICKS;
+	
+	public nThread(String name, Runnable fixRunnable){
+		this.fixRunnable = fixRunnable;
+		setName(name);
+	}
+	
+	public int getTicks(){
+		return TICKS;
+	}
+
+    @Override
+	public void run(){
+    	long lastTime = System.nanoTime();
+    	double nsPerTick = 1000000000/60D;
+    	int ticks = 0;
+    	TICKS = 0;
+    	long lastTimer = System.currentTimeMillis();
+    	double delta_ticks = 0;
+    	boolean shouldUpdate = false;
+    	
+    	while(true){
+			shouldUpdate = false;
+			long now = System.nanoTime();
+			delta_ticks += (now - lastTime)/nsPerTick;
+			lastTime = now;
+			while(delta_ticks >= 1){
+				delta_ticks-=(60.0/maxRate);
+				shouldUpdate = true;
+			}
+			if(shouldUpdate){
+				ticks++;
+				fixRunnable.run();
+			}
+			try{
+				Thread.sleep(2);
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+			if(System.currentTimeMillis()-lastTimer >= 1000){
+				lastTimer+=1000;
+				TICKS = ticks;
+				ticks = 0;
+			}
+		}
+	}
+}
