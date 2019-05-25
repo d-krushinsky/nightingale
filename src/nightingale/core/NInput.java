a7b06778af3107548eff8024b93b4dc99280c5fa
dyuzz16 <ciclopej@gmail.com>
2019-05-25 18:49:20 +0300

    Add key input

---
+++ src/nightingale/core/NInput.java
@@ -0,0 +1,49 @@
+package nightingale.core;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public abstract class NInput implements KeyListener{
+	@Override
+	public void keyPressed(KeyEvent e) {
+		toggleKey(e.getKeyCode(), false);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		toggleKey(e.getKeyCode(), false);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {}	
+	
+	/**
+	 * @param keyCode
+	 * @param isPressed
+	 */
+	public abstract void toggleKey(int keyCode, boolean isPressed);
+	
+	public class Key{
+		private boolean pressed = false;
+		private boolean clicked = false;
+		
+		public boolean isPressed() {
+			return pressed;
+		}
+		
+		public boolean isClicked(){
+			boolean result = false;
+			if(pressed & !clicked) {
+				result = true;
+				clicked = true;
+			}
+			return result;
+		}
+		
+		public void toggle(boolean isPressed) {
+			pressed = isPressed;
+			if(!pressed) clicked = false;
+		}
+	}
+	
+}
