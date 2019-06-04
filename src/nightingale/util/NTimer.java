851c8d66bfc487d2421e5f7f1417cadb3522418a
dyuzz16 <ciclopej@gmail.com>
2019-06-04 21:42:13 +0300

    Add NTimer

---
+++ src/nightingale/util/NTimer.java
@@ -0,0 +1,27 @@
+package nightingale.util;
+
+public class NTimer {
+
+	public double time = 0;
+	
+	public void start() {
+		
+	}
+	
+	public void resume() {
+		
+	}
+	
+	public void stop() {
+		
+	}
+	
+	public double currentTime() {
+		return time;
+	}
+	
+	public void off() {
+		
+	}
+	
+}
